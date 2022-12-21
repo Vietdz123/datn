2d871e1dfdf705ef8ff2761c4e2bc8b5cd26d504
Vietdz123 <108248335+Vietdz123@users.noreply.github.com>
2022-12-21 10:30:23 +0700

    Add Feature Control Group Device

--- test.py
+++ test.py
@@ -8,23 +8,33 @@ import json
 import time
 from datetime import datetime
 
+from threading import Thread
+import threading
+
 broker = '127.0.0.1'
 port = 1883
 topic = "v1/devices/me/rpc/request/+"
 broker="127.0.0.1"   			    #host name
 port=1883 					    #data listening port
-ACCESS_TOKEN='ufgVcklJD3yoKTWSBoa9'    
-# generate client ID with pub prefix randomly
-client_id = f'python-mqtt-{random.randint(0, 100)}'
+ACCESS_TOKEN1='ufgVcklJD3yoKTWSBoa9'    #Smart-lighting -VietPro
+ACCESS_TOKEN2='8rQoOpUxcBmUVkkZaVOb'   # viet
+TELEMETRY = "v1/devices/me/telemetry"
 
-state1 = "false"
-state2 = "false"
+def check_state(fullstring: str, substring: str) :
+    if fullstring.find(substring) != -1:
+        return {"STATE": "ON"}
+    else:
+        return {"STATE": "OFF"}
 
 def connect_mqtt() -> mqtt_client:
-
     client = mqtt_client.Client()  #ko co id
-    client.username_pw_set(ACCESS_TOKEN)
+    client1 = mqtt_client.Client()  #ko co id
+    client.username_pw_set(ACCESS_TOKEN1)
+    client1.username_pw_set(ACCESS_TOKEN2)
+
     client.connect(broker,port,keepalive=60)
+    client1.connect(broker,port,keepalive=60)
+    
     return client
 
 def init_responce() :
@@ -51,8 +61,12 @@ def subscribe(client: mqtt_client):
             payload = payload.replace("\"true\"", "true")
             payload = payload.replace("\"false\"", "false")
             print("PARSE SUCCESSFULLY..................")
-            print(payload)
+
+            payload_telemetry = check_state(payload, "true")
+            payload_telemetry = json.dumps(payload_telemetry)
+            print(payload_telemetry)
             client.publish("v1/devices/me/attributes", payload)
+            client.publish(TELEMETRY, payload_telemetry)
             client.publish(responce, payload)
 
         else :
@@ -72,13 +86,27 @@ def subscribe(client: mqtt_client):
     client.subscribe(topic)
     client.publish("v1/devices/me/attributes", payload)
     
-def run() :
-    client = connect_mqtt()
-    subscribe(client)
-    client.loop_forever()
+def run(client: mqtt_client) :
 
+    client.loop_forever()
+    # client1.loop_forever()
+    print("HAHAHAHAHAH")
 
 
 
 if __name__ == '__main__':
-    run()
+    # client = connect_mqtt()
+    client = mqtt_client.Client()  #ko co id
+    client1 = mqtt_client.Client()  #ko co id
+    client.username_pw_set(ACCESS_TOKEN1)
+    client1.username_pw_set(ACCESS_TOKEN2)
+    client.connect(broker,port,keepalive=60)
+    client1.connect(broker,port,keepalive=60)
+    subscribe(client)
+    subscribe(client1)
+    t1 = threading.Thread(target=run, args=(client,))
+    t2 = threading.Thread(target=run, args=(client1,))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
